6c120e37f826c4c9dc6aed50a14599aa03098459
adesh sengar <81346064+ades-ship@users.noreply.github.com>
2022-04-09 22:52:26 +0530

    fibonacci serie sum

---
+++ folder/fibo.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int fibonacci(int n)
+{
+	if (n == 1 || n==0)
+		return n;
+
+	return fibonacci(n-1)+fibonacci(n-2);
+   
+}
+
+// Driver code
+int main()
+{
+	int r,n;
+    cin>>n;
+	r = fibonacci(n);
+    
+	cout << " fibonacci is " << r;
+
+	return 0;
+}
