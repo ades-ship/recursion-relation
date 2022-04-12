d29071586fec179d3672170ff89a0e86c0bba66e
Adesh sengar <adeshsengar156@gmail.com>
2022-04-12 21:46:29 +0530

    iterative method also

--- folder/fibo.cpp
+++ folder/fibo.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 using namespace std;
 
@@ -8,6 +9,36 @@ int fibonacci(int n)
 
 	return fibonacci(n-1)+fibonacci(n-2);
    
+}
+
+// Driver code
+int main()
+{
+	int r,n;
+    cin>>n;
+	r = fibonacci(n);
+    
+	cout << " fibonacci serie sum is " << r;
+
+	return 0;
+}*/
+
+
+#include <iostream>
+using namespace std;
+
+int fibonacci(int n)
+{  
+	int a=0,b=1,c;
+	if (n == 1 || n==0)
+		return n;
+    for(int i=2; i<n ; i++)
+	{
+       c=a+b;
+	   a=b; 
+	   b=c;
+	}
+  return c;
 }
 
 // Driver code
